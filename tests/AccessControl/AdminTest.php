fd19e3f6acbc03e7d3ca199d3900bd0ff2b5ac70
clement <lionneclement@gmail.com>
2020-05-08 15:15:58 +0200

    create folder and file for check the accessControl

---
+++ tests/AccessControl/AdminTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tests\AccessControl;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class AdminTest extends WebTestCase
+{
+    public function urlFalse()
+    {
+        yield ['/users/create'];
+    }
+    public function urlTrue()
+    {
+        yield ['/'];
+        yield ['/users/14/edit'];
+        yield ['/users/14/edit/password'];
+        yield ['/users/list'];
+        yield ['/admin/users/14/edit'];
+    }
+    /**
+     * @dataProvider urlFalse
+     */
+    public function testFalse($url)
+    {
+        $client = static::createClient();
+        $client->request(
+            'GET', $url, [], [], [
+            'PHP_AUTH_USER' => 'admin@gmail.com',
+            'PHP_AUTH_PW'   => 'password',
+            ]
+        );
+        $this->assertNotEquals(200, $client->getResponse()->getStatusCode());
+    }
+    /**
+     * @dataProvider urlTrue
+     */
+    public function testTrue($url)
+    {
+        $client = static::createClient();
+        $client->request(
+            'GET', $url, [], [], [
+            'PHP_AUTH_USER' => 'admin@gmail.com',
+            'PHP_AUTH_PW'   => 'password',
+            ]
+        );
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+}
---
+++ tests/AccessControl/UserTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tests\AccessControl;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class UserTest extends WebTestCase
+{
+    public function urlFalse()
+    {
+        yield ['/users/create'];
+        yield ['/users/list'];
+        yield ['/admin/users/14/edit'];
+    }
+    public function urlTrue()
+    {
+        yield ['/'];
+        yield ['/users/14/edit'];
+        yield ['/users/14/edit/password'];
+    }
+    /**
+     * @dataProvider urlFalse
+     */
+    public function testFalse($url)
+    {
+        $client = static::createClient();
+        $client->request(
+            'GET', $url, [], [], [
+            'PHP_AUTH_USER' => 'user@gmail.com',
+            'PHP_AUTH_PW'   => 'password',
+            ]
+        );
+        $this->assertNotEquals(200, $client->getResponse()->getStatusCode());
+    }
+    /**
+     * @dataProvider urlTrue
+     */
+    public function testTrue($url)
+    {
+        $client = static::createClient();
+        $client->request(
+            'GET', $url, [], [], [
+            'PHP_AUTH_USER' => 'user@gmail.com',
+            'PHP_AUTH_PW'   => 'password',
+            ]
+        );
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+}
