fdcdbf1178d2dd9059f7579ab3e16f736f3360ae
clement <lionneclement@gmail.com>
2020-05-09 16:15:58 +0200

    change the name of the group

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -19,7 +19,7 @@ class AppFixtures extends Fixture implements FixtureGroupInterface
     }
     public static function getGroups(): array
     {
-        return ['data'];
+        return ['app'];
     }
     public function load(ObjectManager $manager)
     {
