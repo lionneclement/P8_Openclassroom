4fd0bf79b6274adcda25764e956fc925dc66a09b
clement <lionneclement@gmail.com>
2020-05-18 19:18:28 +0200

    update testing in userController

--- tests/Controller/UserController/EditProfilTest.php
+++ tests/Controller/UserController/EditProfilTest.php
@@ -6,6 +6,22 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class EditProfilTest extends WebTestCase
 {
+    private $client;
+
+    public function setUp(): void
+    {
+        $this->client = static::createClient();
+        $this->client->followRedirects();
+    }
+    public function login($url)
+    {
+        return $this->client->request(
+            'GET', $url, [], [], [
+            'PHP_AUTH_USER' => 'testEdit@gmail.com',
+            'PHP_AUTH_PW'   => 'password',
+              ]
+        );
+    }
     public function url()
     {
         yield ['/users/edit'];
@@ -15,21 +31,27 @@ class EditProfilTest extends WebTestCase
      */
     public function testSuccessForm($url)
     {
-        $client = static::createClient();
-        $client->followRedirects();
+        $crawler = $this->login($url);
+        $form = $crawler->selectButton('Modifier')->form();
+
+        $form['profil[username]'] = 'testEdit';
+        $form['profil[email]'] = 'testEdit@gmail.com';
+        $crawler = $this->client->submit($form);
         
-        $crawler = $client->request(
-            'GET', $url, [], [], [
-            'PHP_AUTH_USER' => 'testEdit@gmail.com',
-            'PHP_AUTH_PW'   => 'password',
-              ]
-        );
+        $this->assertGreaterThan(0, $crawler->filter('div.alert-success')->count());
+    }
+    /**
+     * @dataProvider url
+     */
+    public function testErrorAlreadyEmailUsedForm($url)
+    {
+        $crawler = $this->login($url);
         $form = $crawler->selectButton('Modifier')->form();
 
         $form['profil[username]'] = 'editTest';
-        $form['profil[email]'] = 'editTest@gmail.com';
-        $crawler = $client->submit($form);
+        $form['profil[email]'] = 'user@gmail.com';
+        $crawler = $this->client->submit($form);
         
-        $this->assertGreaterThan(0, $crawler->filter('div.alert-success')->count());
+        $this->assertGreaterThan(0, $crawler->filter('span.help-block')->count());
     }
 }
